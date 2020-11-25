fd419fa4b94d72ffc302b8cad9b04c94c4147f0e
Sergei Blinov <blinovsv@gmail.com>
2020-11-25 16:07:27 +0300

    huge tmux + alacritty integration

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ set -e
 DOTFILES_DIR=$(pwd)
 
 (cd ~; rm -rf .gitconfig; ln -s $DOTFILES_DIR/.gitconfig .gitconfig)
+(cd ~/.config/alacritty; rm -rf alacritty.yml; ln -s $DOTFILES_DIR/alacritty.yml alacritty.yml)
+(cd ~; rm -rf .tmux.conf; ln -s $DOTFILES_DIR/.tmux.conf .tmux.conf)
