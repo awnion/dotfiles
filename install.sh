80eab9f9b33085988c7352d1002d2992f2a4f219
Sergei Blinov <blinovsv@gmail.com>
2021-01-16 23:24:30 +0300

    huge refactoring

--- install.sh
+++ install.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
+# TODO: Maybe rewrite this on python??
+#       check this: https://github.com/anishathalye/dotbot/blob/master/bin/dotbot
+
 set -e
 DOTFILES_DIR=$(pwd)
 
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
-    # enable key repeating in specific apps on MacOS
-    defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
-    defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
-    defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
-    # defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
+  # Vim mode vscode and others
+  # enable key repeating in specific apps on MacOS
+  defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+  defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+  defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
+  # defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
 fi
 
-(cd $HOME; rm -rf .gitconfig; ln -s $DOTFILES_DIR/.gitconfig .gitconfig)
-(cd $HOME/.config/alacritty; rm -rf alacritty.yml; ln -s $DOTFILES_DIR/alacritty.yml alacritty.yml)
-(cd $HOME; rm -rf .tmux.conf; ln -s $DOTFILES_DIR/tmux.conf .tmux.conf)
-(cd $HOME; rm -rf .vimrc; ln -s $DOTFILES_DIR/vimrc .vimrc)
-(cd $HOME; rm -rf .zshrc; ln -s $DOTFILES_DIR/zshrc .zshrc)
-(cd $HOME; rm -rf .zprofile; ln -s $DOTFILES_DIR/zprofile .zprofile)
-(cd $HOME/.config; rm -rf wezterm; ln -s $DOTFILES_DIR/wezterm wezterm)
+(
+  cd $HOME
+  rm -rf .gitconfig
+  rm -rf .tmux.conf
+  rm -rf .vimrc
+  rm -rf .zprofile
+  rm -rf .zshrc
+  ln -s $DOTFILES_DIR/gitconfig .gitconfig
+  ln -s $DOTFILES_DIR/tmux.conf .tmux.conf
+  ln -s $DOTFILES_DIR/vimrc .vimrc
+  ln -s $DOTFILES_DIR/zprofile .zprofile
+  ln -s $DOTFILES_DIR/zshrc .zshrc
+)
+
+mkdir -p $HOME/.config
+(
+  cd $HOME/.config
+  rm -rf alacritty
+  rm -rf wezterm
+  ln -s $DOTFILES_DIR/config/alacritty alacritty
+  ln -s $DOTFILES_DIR/config/wezterm wezterm
+)
