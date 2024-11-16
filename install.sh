f19d06f24fe912e1b5c5f3a0a7f68bdfed642a0b
Sergei Blinov <blinovsv@gmail.com>
2024-11-16 09:40:49 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
   defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
   defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
   defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
-  # defaults delete -g ApplePressAndHoldEnabled                                      # If necessary, reset global default
+  defaults write -g ApplePressAndHoldEnabled -bool false                           # For global
+  # defaults delete -g ApplePressAndHoldEnabled                                    # If necessary, reset global default
 
   # make repeat speed lighting fast
   defaults write -g InitialKeyRepeat -int 15 # normal minimum is 15 (225 ms)
